9fe56ead06a84699c34c39e2062948d2b8093df6
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2022-06-02 16:07:45 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './components/header/Header'
 import Sb from './components/sb/Sb'
+import SignIn from './components/signin/SignIn';
 
 
 class App extends Component
@@ -10,9 +12,14 @@ class App extends Component
         return(
             <>
                 <Sb />
-                <div className = '_header'>
-                    <Header />
-                </div>
+                <Router>
+                  <Routes>
+                    <Route path="/" element={<Header />} />
+                    <Route path="/signin" element={<SignIn />} />
+
+                  </Routes>
+                </Router>
+                
             </>
         )
     }
